0a39156a42c262a48a920405eb4b09382d4e43cc
wkailin <85991853+wkailin@users.noreply.github.com>
2021-12-16 17:13:59 -0500

    Update normMixEm.R
    Add docs

--- R/normMixEm.R
+++ R/normMixEm.R
@@ -1,35 +1,31 @@
-#'normMixEm
-#'
-#' ?Description
-#'
-#'@param input_dat n x p data matrix
-#'
-#'@param num_components number of clusters
-#'
-#'@return A list  
-#'\itemize{
-#'   \item convergence
-#'   \item mu_mat
-#'   \item sigma_mat
-#'   \item pi_vec
-#'   \item iter
-#'   \item loglik+list
-#'   \item prob_mat
+#' @name normMixEm
+#' @title EM Algorithm for Mixtures of Independent Multivariate Normals
+#' @description  This is a normMixEm class, which initializes and runs EM algorithm output for mixtures of independent multivariate normal distributions
+#' @param input_data  A matrix/dataframe of size nxp consisting of the data.
+#' @param num_components Number of components.
+#' @return A list including
+#' \itemize{
+#'   \item convergence - Convergece status. 0 if converged,1 if not.
+#'   \item mu_mat - A k*p matrix includes the mean parameters of k normals.
+#'   \item sigma_mat - A k*p matrix includes the variance parameters of k normals.
+#'   \item pi_vec - A vector of length k includes the probability parameters of k components.
+#'   \item iter - A integer. Number of iterations
+#'   \item loglik_list - A number. The log-likelihood of the output result
+#'   \item prob_mat - A n*k matrix includes the probabilities of being assigned to k components for each sample.
 #'}
-#'
-#'@import mixtools
-#'@import dplyr
-#'@import methods
-#'@export 
-#'
-#'
+#' @examples
+#'  EM <- normMixEm$new(input_dat = data,num_components = num_components) ## initialization with $new
+#'  res_EM <- EM$run.EM(loglik_tol=1e-5) ## run the main function for EM algorithm with $run.EM
+#' @import stats
+#' @import mixtools
+#' @import mvtnorm
+#' @export
 
 
-#### DONNOT MODIFY THIS PART 
-#### RUN THIS CHUNK BEFORE USING 'normMixEm ' FOR CLUSTERING
 
-## My EM algorithm: Multivariate(independent) norm()
-normMixEM <- setRefClass("normMixEM",
+#' Create a class normMixEM
+#' method to initialize normMixEm class (called with normMixEm$new)
+normMixEm <- setRefClass("normMixEM",
                          fields=list(k = "integer",
                                      n = "integer",
                                      dat_mat = "matrix",
@@ -40,45 +36,118 @@ normMixEM <- setRefClass("normMixEM",
                                      loglik = "numeric",
                                      tol = "numeric"))
 
-
-#method to initialize normMixEm class (called with normMixEm$new)
-normMixEM$methods(initialize = function(input_dat,num_components){
+normMixEm$methods(initialize = function(input_dat,num_components){
   dat_mat <<- input_dat ## Use <<- to assign fields
   k <<- num_components
   n <<- dim(dat_mat)[1]
   p <<- dim(dat_mat)[2]
 })
 
-
-#Sample from a multivariate normal distribution based on cholesky decomposition
-# Alternative version of rmvnorm to eliminate dependence of mixtools
-# on additional package 'mvtnorm'
-#Sample from a multivariate normal distribution based on cholesky decomposition
-
-rmvnorm_chol <- function(n, mu=NULL, sigma=NULL) {
-  if (is.null(mu)) {
-    if (is.null(sigma)) {
-      return(rnorm(n)) # return standard norm
-    } else {
-      mu = rep(0, nrow(sigma))
-    }
-  } else if (is.null(sigma)) {
-    sigma=diag(length(mu))
-  }
-  p <- length(mu)
-  if (p != nrow(sigma) || p != ncol(sigma)) 
-    stop("length of mu must equal nrow and ncol of sigma")
-  Z <- matrix(rnorm(n*p),nrow=p,ncol=n)
-  try(U <- chol(sigma))
-  X <- mu + crossprod(U, Z)
-  return(t(X))
-}
-
 # dat_mat: n * p 
 # mu_mat: k * p
 # sigma_mat: k * p
 # prob_mat: n * k
-# rmvnorm_chol
+
+#' method to initialize parameters for E-M
+normMixEm$methods(init.paras = function(){
+  tol <<- 1e-100  ## small number to avoid log(zero) 
+  init   <<- normalmix_init(dat_mat, k = k)
+  pi_vec <<- init$lambda  # k*1
+  mu_mat <<- init$mu # k*p 
+  sigma_mat <<- init$sigma # k*p
+  prob_mat <<- matrix(NA,nrow=n,ncol=k)
+  loglik <<- -Inf
+})
+
+
+#' E-step for E-M algorithm
+normMixEm$methods(update.prob = function(){
+  ## prob_mat contains log-likelihood of each components
+  # version 1
+  prob_mat <<- sapply(1:k, function(j)
+    log(pi_vec[j]+tol) + logdmvnorm(dat_mat, mu_mat[j,], sigma_mat[j,])
+  )
+  ## version 2
+  # prob_mat <<- sapply(1:k, function(j)
+  #   log(pi_vec[j]+tol) + log(mvtnorm::dmvnorm(dat_mat, mu_mat[j,], diag(sigma_mat[j,]))+tol)
+  # ) #  mvtnorm::dmvnorm is more stable!!
+  max_log_prob <- apply(prob_mat,1,max) ## local change, do not change the field value
+  prob_mat <<- exp(prob_mat - max_log_prob) ## re-scale probability (important)
+  sum_prob <- apply(prob_mat,1,sum)   ## sum probabilities for each observation
+  prob_mat <<- prob_mat/sum_prob      ## normalize the probability E[z|theta]
+  loglik <<- sum(max_log_prob + log(sum_prob))  ## evaluate log-likelihood
+})
+
+#' M-step for E-M algorithm to update pi_vec
+normMixEm$methods(update.pi = function(){
+  pi_vec <<- apply(prob_mat,2,mean)# prob_mat:  n*k => k*1
+})
+
+#' M-step for E-M algorithm to update mu_mat
+normMixEm$methods(update.mu = function(){
+  # mu_mat: k * p
+  mu_mat <<- (crossprod(prob_mat,dat_mat)/(apply(prob_mat,2,sum)+tol))#apply(dat_mat*prob_mat,2,mean)/(pi_vec+tol)
+})
+
+#' M-step for E-M algorithm to update sigma_mat
+normMixEm$methods(update.sigma = function(){
+  # sigma_mat: k * p
+  sigma_mat <<- t(sapply(1:k, function(i) crossprod(prob_mat[,i], 
+                                                    t(t(dat_mat)- mu_mat[i,])^2)
+  ))
+  ## update sigma as weighted average
+  sigma_mat <<- sigma_mat/(n*pi_vec+tol)
+})
+
+
+#' check convergence of mixture normal EM
+normMixEm$methods(check.tol = function(fmax,fmin,ftol){
+  delta = abs(fmax - fmin)
+  accuracy = (abs(fmax) + abs(fmin))*ftol
+  return(delta < (accuracy + tol))
+})
+
+#' main function for E-M algorithm
+normMixEm$methods(run.EM = function(max_iter=1000L,loglik_tol=1e-5){
+  convergence = 1L
+  init.paras()  ## initialize parameter
+  loglik_list = NULL
+  for(iter in 1:max_iter){
+    loglik0 <- loglik ## log-likelihood of previous steps
+    update.prob() # E-step
+    update.pi()   # M-step for pi_vec
+    update.mu()   # M-step for mu_mat
+    update.sigma() # M-step for sigma_mat
+    loglik_list = c(loglik_list,loglik) # append log-likelihood
+    if(check.tol(loglik0,loglik,loglik_tol)){
+      convergence = 0 # converged
+      break
+    }
+  }
+  return(list(convergence=convergence,mu_mat=mu_mat,sigma_mat=sigma_mat,
+              pi_vec=pi_vec,iter=iter,loglik_list=loglik_list, prob_mat=prob_mat))
+})
+
+
+
+#'@name normalmix_init
+#'@title Initialization for normMixEm Algorithm
+#'@description Internal initialization function for normMixEm algorithm in this package. Modified based on the normalmix.init{mixtools}.
+#'
+#'@param x A matrix of size n*p consisting of the data.
+#'@param k Number of components. Default as 5.
+#'
+#'@return A list of parameters for initialization including:
+#' \itemize{
+#'   \item lambda - A vector of length k includes the probability parameters of k components.
+#'   \item mu - A k*p matrix includes the mean parameters of k Normals.
+#'   \item sigma - A k*p matrix includes the variance parameters of k Normals.
+#'   \item k - Integer. Number of components.  
+#'}
+#'
+#'@import stats
+#'@export 
+#'
 normalmix_init = function (x, k = 5) 
 {
   n <- nrow(x)
@@ -112,81 +181,37 @@ normalmix_init = function (x, k = 5)
   return(list(lambda = lambda, mu = mu, sigma = sigma, k = k))
 }
 
-#method to initialize parameters for E-M
-normMixEM$methods(init.paras = function(){
-  tol <<- 1e-100  ## small number to avoid log(zero) 
-  init   <<- normalmix_init(dat_mat, k = k)
-  pi_vec <<- init$lambda  # k*1
-  mu_mat <<- init$mu # k*p 
-  sigma_mat <<- init$sigma # k*p
-  prob_mat <<- matrix(NA,nrow=n,ncol=k)
-  loglik <<- -Inf
-})
 
+#'@name rmvnorm_chol
+#'@title Simulate from a Multivariate Normal Distribution
+#'@description Sample from a multivariate normal distribution based on Cholesky decomposition. Modified based on rmvnorm {mixtools}
+#'
+#'@param n Number of vectors to simulate.
+#'@param mu Mean vector.
+#'@param sigma Covariance matrix, assumed symmetric and nonnegative definite.
+#'
+#'@return A n*p matrix. Each row is a multivariate normal vector generated independently based on the input parameters.
+#'
+#'@import stats
+#'@export 
+#'
 
-#E-step for E-M algorithm
-normMixEM$methods(update.prob = function(){
-  prob_mat <<- sapply(1:k, function(j)
-    log(pi_vec[j]+tol) + logdmvnorm(dat_mat, mu_mat[j,], diag(sigma_mat[j,]))
-  ) #  logdmvnorm(dat_mat, mu_mat[j,] vector, diag(sigma_mat[j,]) matrix)
-  
-  max_log_prob <- apply(prob_mat,1,max) ## local change, do not change the field value
-  prob_mat <<- exp(prob_mat - max_log_prob) ## re-scale probability (important)
-  sum_prob <- apply(prob_mat,1,sum)   ## sum probabilities for each observation
-  prob_mat <<- prob_mat/sum_prob      ## normalize the probability E[z|theta]
-  loglik <<- sum(max_log_prob + log(sum_prob))  ## evaluate log-likelihood
-})
-
-#M-step for E-M algorithm to update pi_vec
-normMixEM$methods(update.pi = function(){
-  pi_vec <<- apply(prob_mat,2,mean)# prob_mat:  n*k => k*1
-})
-
-#M-step for E-M algorithm to update mu_mat
-normMixEM$methods(update.mu = function(){
-  # mu_mat: k * p
-  mu_mat <<- (crossprod(prob_mat,dat_mat)/(apply(prob_mat,2,sum)+tol))#apply(dat_mat*prob_mat,2,mean)/(pi_vec+tol)
-})
-
-#M-step for E-M algorithm to update sigma_mat
-normMixEM$methods(update.sigma = function(){
-  # sigma_mat: k * p
-  sigma_mat <<- t(sapply(1:k, function(i) crossprod(prob_mat[,i], 
-                                                    t(t(dat_mat)- mu_mat[i,])^2)
-  ))
-  ## update sigma as weighted average
-  sigma_mat <<- sigma_mat/(n*pi_vec+tol)
-})
-
-
-#check convergence of mixture normal EM
-normMixEM$methods(check.tol = function(fmax,fmin,ftol){
-  delta = abs(fmax - fmin)
-  accuracy = (abs(fmax) + abs(fmin))*ftol
-  return(delta < (accuracy + tol))
-})
-#main function for E-M algorithm
-
-normMixEM$methods(run.EM = function(max_iter=1000L,loglik_tol=1e-5){
-  convergence = 1L
-  init.paras()  ## initialize parameter
-  loglik_list = NULL
-  for(iter in 1:max_iter){
-    loglik0 <- loglik ## log-likelihood of previous steps
-    update.prob() # E-step
-    update.pi()   # M-step for pi_vec
-    update.mu()   # M-step for mu_mat
-    update.sigma() # M-step for sigma_mat
-    loglik_list = c(loglik_list,loglik) # append log-likelihood
-    if(check.tol(loglik0,loglik,loglik_tol)){
-      convergence = 0 # converged
-      break
+# Alternative version of rmvnorm to eliminate dependence of mixtools
+rmvnorm_chol <- function(n, mu=NULL, sigma=NULL) {
+  if (is.null(mu)) {
+    if (is.null(sigma)) {
+      return(rnorm(n)) # return standard norm
+    } else {
+      mu = rep(0, nrow(sigma))
     }
+  } else if (is.null(sigma)) {
+    sigma=diag(length(mu))
   }
-  return(list(convergence=convergence,mu_mat=mu_mat,sigma_mat=sigma_mat,
-              pi_vec=pi_vec,iter=iter,loglik_list=loglik_list, prob_mat=prob_mat))
-})
-
-
-
-
+  p <- length(mu)
+  if (p != nrow(sigma) || p != ncol(sigma)) 
+    stop("length of mu must equal nrow and ncol of sigma")
+  Z <- matrix(rnorm(n*p),nrow=p,ncol=n)
+  try(U <- chol(sigma))
+  X <- mu + crossprod(U, Z)
+  return(t(X))
+}
